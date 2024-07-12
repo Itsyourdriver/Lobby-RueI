93b39878e7c1abf7e644b7da587b808e4074d6f1
Itsyourdriver <73563507+Itsyourdriver@users.noreply.github.com>
2024-07-11 18:13:25 -0700

    RueI support

--- Lobby/Lobby.cs
+++ Lobby/Lobby.cs
@@ -1,22 +1,30 @@
-﻿namespace Lobby
+namespace Lobby
 {
     using PluginAPI.Core.Attributes;
+    using PluginAPI.Core;
     using PluginAPI.Enums;
     using PluginAPI.Events;
+    using RueI;
 
     public class Lobby
     {
         public static Lobby Instance { get; private set; }
 
         [PluginConfig("configs/lobby.yml")]
-        public static Config Config;
+        public Config Config;
 
         [PluginPriority(LoadPriority.Highest)]
-        [PluginEntryPoint("Lobby", "1.2.8", "A plugin that adds a lobby when waiting for players.", "MrAfitol")]
-        public void LoadPlugin()
+        [PluginEntryPoint("Lobby", "1.1.3", "A plugin that adds a lobby when waiting for players.", "MrAfitol")]
+        void LoadPlugin()
         {
             Instance = this;
             EventManager.RegisterEvents<EventHandlers>(this);
+            Log.Info("Loading Lobby...");
+            var handler = PluginHandler.Get(this);
+            handler.SaveConfig(this, nameof(Config));
+            Log.Info("Lobby has loaded...");
+            RueIMain.EnsureInit();
+            Log.Info("Loaded RueI...");
         }
     }
 }
